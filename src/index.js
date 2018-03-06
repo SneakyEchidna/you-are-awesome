5bb7a52550f2bf7bd8358d71eb2c2a9d893db89a
Kanstantsin Kiyko <xfozzyx@gmail.com>
2018-03-06 21:10:23 +0300

    5 done

--- src/index.js
+++ src/index.js
@@ -6,15 +6,25 @@ const createEnumerableProperty = (propertyName) => {
 const createNotEnumerableProperty = (propertyName) => {
   return Symbol(propertyName)
 };
-const createProtoMagicObject = () => { };
+const createProtoMagicObject = () => {
+  let f = function () { }; f.__proto__ = f.prototype; return f
+};
 const incrementor = () => { };
 const asyncIncrementor = () => { };
 const createIncrementer = () => { };
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => { };
+const returnBackInSecond = (input) => {
+  return new Promise((resolve) => setTimeout(resolve, 1001, input))
+};
 const getDeepPropertiesCount = () => { };
-const createSerializedObject = () => { };
+const createSerializedObject = () => {
+  let o = new String();
+  //this.valueOf = () => {
+  //  return this.toString()
+  //  }
+  return o
+};
 const toBuffer = () => { };
 const sortByProto = () => { };
 
