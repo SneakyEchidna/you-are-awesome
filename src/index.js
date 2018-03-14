f34a355994da3b9b63135b4565a6d78a3f6d5c22
Kanstantsin Kiyko <xfozzyx@gmail.com>
2018-03-14 14:56:28 +0300

    two more done

--- src/index.js
+++ src/index.js
@@ -9,20 +9,34 @@ const createNotEnumerableProperty = (propertyName) => {
 const createProtoMagicObject = () => {
   let f = function () { }; f.__proto__ = f.prototype; return f
 };
-const incrementor = () => { };
+const incrementor = () => {
+};
 const asyncIncrementor = () => { };
-const createIncrementer = () => { };
+const createIncrementer = function* () {
+  let counter = 0
+  while (true) {
+    yield ++counter
+  }
+};
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = (input) => {
   return new Promise((resolve) => setTimeout(resolve, 1001, input))
 };
-const getDeepPropertiesCount = () => { };
+const getDeepPropertiesCount = (obj) => {
+  let ownPropCount = 0
+  for (let property in obj) {
+    if (obj.hasOwnProperty(property)) {
+      ownPropCount++
+      if (typeof obj[property] === 'object') {
+        ownPropCount = ownPropCount + getDeepPropertiesCount(obj[property])
+      }
+    }
+  }
+  return ownPropCount
+};
 const createSerializedObject = () => {
   let o = new String();
-  //this.valueOf = () => {
-  //  return this.toString()
-  //  }
   return o
 };
 const toBuffer = () => { };
